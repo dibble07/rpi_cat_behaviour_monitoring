d571e32f561d1c8a0e2fa27a1c6a4cba561a3223
Robert Dibble <robertdibble@live.co.uk>
2025-12-30 16:25:43 +0000

    Add pi specific settings and camera object

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
-from config import settings
+from camera import get_camera
+from config import SYSTEM, settings
 
 logger = logging.getLogger(__name__)
 
@@ -36,7 +37,8 @@ def draw_detections(
         (w, h), _ = cv2.getTextSize(label, FONT, 1, 1)
         txt_coords = (int(x1 + w * 0.05), int(y1 + h * 1.1))
         txt_box_coords = (int(x1 + 1.1 * w), int(y1 + 1.2 * h))
-        cv2.rectangle(frame, (x1, y1), (x2, y2), ANN_COLOUR, 2)
+        thickness = int(min(frame.shape[:2]) / 250)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), ANN_COLOUR, thickness)
         cv2.rectangle(frame, (x1, y1), txt_box_coords, ANN_COLOUR, -1)
         cv2.putText(frame, label, txt_coords, FONT, 1, (255, 255, 255))
 
@@ -48,13 +50,9 @@ os.makedirs(settings.OUTPUT_DIR, exist_ok=True)
 model = YOLO(settings.MODEL_PATH, task="detect")
 
 # prepare camera and buffer
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FPS, settings.TARGET_FPS)
-fps = cap.get(cv2.CAP_PROP_FPS)
-if fps != settings.TARGET_FPS:
-    logger.warning(f"Camera FPS ({fps}) does not match target ({settings.TARGET_FPS})")
-frame_int = 1 / fps
-pre_buffer = collections.deque(maxlen=int(settings.BUFFER_DUR * fps))  # type: ignore
+cam = get_camera()
+frame_int = 1 / cam.fps
+pre_buffer = collections.deque(maxlen=int(settings.BUFFER_DUR * cam.fps))  # type: ignore
 
 # initialise state
 recording = False
@@ -66,7 +64,7 @@ while True:
 
     # store current frame image and timestamp to rolling buffer
     t0 = datetime.now()
-    _, frame = cap.read()
+    frame = np.ascontiguousarray(cam())
     pre_buffer.append((t0, frame.copy()))
 
     # identify objects in current frame
@@ -87,7 +85,7 @@ while True:
             out_path = os.path.join(
                 settings.OUTPUT_DIR, f"{t0.strftime('%Y%m%d_%H%M%S')}.mp4"
             )
-            writer = cv2.VideoWriter(out_path, FOURCC, fps, frame.shape[:2][::-1])
+            writer = cv2.VideoWriter(out_path, FOURCC, cam.fps, frame.shape[:2][::-1])
             for _, bf in pre_buffer:
                 writer.write(bf)
             recording = True
@@ -104,12 +102,13 @@ while True:
             recording = False
 
     # manual closing of app and recording
-    cv2.imshow("Object monitor (q to quit)", frame)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        if writer is not None:
-            writer.release()
-            logger.warning(f"Saved clip: {out_path}")
-        break
+    if SYSTEM == "Darwin":
+        cv2.imshow("Object monitor (q to quit)", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            if writer is not None:
+                writer.release()
+                logger.warning(f"Saved clip: {out_path}")
+            break
 
     # delay next processing to match camera frame rate
     delay = (timedelta(seconds=frame_int) - (datetime.now() - t0)).total_seconds()
