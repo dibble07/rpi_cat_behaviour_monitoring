4edc1b2f0280aba0e4889c24fa8f4af16fdca737
Robert Dibble <robertdibble@live.co.uk>
2026-02-05 21:38:08 +0100

    restructure to a file per thread

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
+from __future__ import annotations
+
 import faulthandler
 import logging
 import os
 import queue
-import signal
 import threading
 import time
 from datetime import datetime
 
 import cv2
-import numpy as np
-import psutil
 
-import utils
-from camera import get_camera
+from capture import capture_thread
 from config import SYSTEM, settings
+from monitoring import monitoring_thread
+from processing import processing_thread
+from shared import display_queue, shutdown_event
 
 logging.basicConfig(
     level=logging.INFO,
@@ -32,210 +33,10 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-process = psutil.Process(os.getpid())
 faulthandler.enable()
-
-
-def _handle_exit(signum, _):
-    """Store global shutdown request flag"""
-    logger.info(f"Received signal {signum} to shut down")
-    shutdown_event.set()
-
-
-# set constants
-FOURCC = cv2.VideoWriter_fourcc(*"mp4v")  # type: ignore
-
-# prep terminal shutdown
-shutdown_event = threading.Event()
-signal.signal(signal.SIGINT, _handle_exit)
-signal.signal(signal.SIGTERM, _handle_exit)
 
 # prepare output directory
 os.makedirs(settings.OUTPUT_DIR, exist_ok=True)
-
-# prepare threadsafe queues
-frame_queue: queue.Queue[tuple[datetime, np.ndarray]] = queue.Queue()
-display_queue: queue.Queue[np.ndarray] = queue.Queue(maxsize=1)
-
-# prepare camera
-cam = get_camera()
-
-
-def capture_thread():
-    """Continuously capture camera frames and add to queue"""
-    logger.info("Capture thread started")
-
-    # set capture constants
-    frame_period = 1 / cam.fps
-
-    while not shutdown_event.is_set():
-
-        # get image and timestamp
-        timestamp = datetime.now()
-        image = cam()
-
-        # shutdown if mock camera reached end of file
-        if cam._mock and image is None:
-            logger.warning("Mock camera reached end of file")
-            shutdown_event.set()
-            continue
-
-        # enqueue the frame
-        frame_queue.put((timestamp, image))
-
-        # maintain camera frame rate
-        elapsed = (datetime.now() - timestamp).total_seconds()
-        delay = frame_period - elapsed
-        logger.debug(f"Capture duration: {elapsed*1000:.1f} ms")
-        if delay > 0:
-            logger.debug(f"Capture delayed to maintain frame rate: {delay*1000:.3f} ms")
-            time.sleep(delay)
-        else:
-            logger.warning(f"Capture thread slow: {1/elapsed:.1f} FPS")
-
-    logger.info("Capture thread stopped")
-
-
-def processing_thread():
-    """Process frames to detect objects and record videos"""
-    logger.info("Processing thread started")
-
-    # initialise preroll buffer
-    pre_buffer = utils.PreBuffer(max_duration=settings.BUFFER_DUR)
-
-    # initialise state and previous frame
-    recording = False
-    writer = None
-    prev_image_grey_blur = np.zeros(
-        (settings.FRAME_HEIGHT, settings.FRAME_WIDTH), dtype=np.uint8
-    )
-    prev_object_detections = []  # type: ignore
-
-    while not shutdown_event.is_set() or not frame_queue.empty():
-
-        # get frame from capture queue
-        try:
-            timestamp, image = frame_queue.get(timeout=0.1)
-            frame = utils.Frame(
-                timestamp=timestamp,
-                image=image,
-                prev_image_grey_blur=prev_image_grey_blur,
-                prev_object_detections=prev_object_detections,
-            )
-        except queue.Empty:
-            continue
-
-        # start timing
-        logger.debug(f"({frame.hash}) Running processing")
-        start = datetime.now()
-
-        if frame.object_detections:
-
-            if frame.has_excluded_class:
-
-                # clear buffer
-                pre_buffer.frames.clear()
-                logger.info("Clearing buffer due to detection of excluded class")
-
-            else:
-
-                # update latest detection timestamp
-                last_detection_time = frame.timestamp
-
-                # initialise recording and write pre buffer to video file
-                if not recording:
-                    out_path = os.path.join(
-                        settings.OUTPUT_DIR,
-                        f"{frame.timestamp.strftime('%Y%m%d_%H%M%S')}.mp4",
-                    )
-                    writer = cv2.VideoWriter(
-                        out_path, FOURCC, cam.fps, frame.image.shape[:2][::-1]
-                    )
-                    logger.warning(f"Starting recording: {out_path}")
-                    pre_buffer_len = len(pre_buffer)
-                    for bf in pre_buffer:
-                        writer.write(bf.image_annotated)
-                    logger.info(
-                        f"Written {pre_buffer_len} frames from pre detection buffer"
-                    )
-                    recording = True
-
-        if recording:
-
-            # write current frame and assess post buffer termination
-            if not frame.has_excluded_class:
-                writer.write(frame.image_annotated)
-                last_detection_dur = (
-                    frame.timestamp - last_detection_time
-                ).total_seconds()
-
-            # stop recording close video file
-            if (last_detection_dur > settings.BUFFER_DUR) or frame.has_excluded_class:
-                writer.release()
-                if last_detection_dur > settings.BUFFER_DUR:
-                    logger.info(
-                        f"Saving clip: last detection was {last_detection_dur:.3f} ago"
-                    )
-                elif frame.has_excluded_class:
-                    logger.info(f"Saving clip: excluded class detected")
-                recording = False
-
-        else:
-
-            # store current frame image and timestamp to rolling buffer
-            if not frame.has_excluded_class:
-                pre_buffer.put(frame)
-
-        # send frame to display queue
-        if SYSTEM == "Darwin":
-            try:
-                display_queue.put_nowait(frame.image_annotated.copy())
-            except queue.Full:
-                pass
-
-        # update current frame to be previous frame
-        prev_image_grey_blur = frame.prev_image_grey_blur.copy()
-        prev_object_detections = frame.prev_object_detections.copy()
-
-        # log processing rate
-        elapsed = (datetime.now() - start).total_seconds()
-        processing_fps = 1 / elapsed
-        logger.debug(f"({frame.hash}) Processing duration: {elapsed*1000:.1f} ms")
-        if processing_fps < cam.fps:
-            logger.warning(f"Processing thread slow: {processing_fps:.1f} FPS")
-
-    # cleanup
-    if writer is not None:
-        writer.release()
-        logger.info("Saving clip")
-
-    logger.info("Processing thread stopped")
-
-
-def monitoring_thread():
-    # prepare percent sampling
-    psutil.cpu_percent(percpu=True)
-    process.cpu_percent()
-
-    while not shutdown_event.is_set():
-        # memory usage
-        rss = process.memory_info().rss / (1024 * 1024)
-        logger.info(f"Memory: {rss:.0f} MB")
-
-        # CPU usage
-        proc_cpu = process.cpu_percent(interval=None)
-        logger.info(f"ProcCPU: {proc_cpu:.1f}%")
-
-        # thread counts
-        num_threads = process.num_threads()
-        logger.info(f"Threads:{num_threads}")
-
-        # queue size
-        q_len = frame_queue.qsize()
-        logger.info(f"Frame queue length: {q_len}")
-
-        time.sleep(0.5)
-
 
 # start threads
 capture_t = threading.Thread(target=capture_thread)
---
+++ processing.py
@@ -0,0 +1,355 @@
+import hashlib
+import logging
+import os
+import queue
+from datetime import datetime, timedelta
+from typing import List, Union
+
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+from config import SYSTEM, settings
+from shared import cam, display_queue, frame_queue, shutdown_event
+
+logger = logging.getLogger(__name__)
+
+# annotation values
+ANN_COLOUR = (0, 200, 0)
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+
+# video type
+FOURCC = cv2.VideoWriter_fourcc(*"mp4v")  # type: ignore
+
+# load object detection model
+MODEL = YOLO(settings.MODEL_PATH, task="detect")
+
+# define background subtractor
+BACK_SUB = cv2.createBackgroundSubtractorMOG2(
+    history=settings.BACKGROUND_HISTORY, detectShadows=False
+)
+
+
+class Frame:
+    """Store frame image and timestamp as well as supplementary processing and annotations"""
+
+    def __init__(
+        self,
+        timestamp: datetime,
+        image: np.ndarray,
+        prev_image_grey_blur: np.ndarray,
+        prev_object_detections: List,
+    ) -> None:
+        self.timestamp = timestamp
+        self.image = np.ascontiguousarray(image)
+        self.hash = hashlib.md5(image.tobytes()).hexdigest()[:6]
+        self.prev_image_grey_blur = prev_image_grey_blur
+        self.prev_object_detections = prev_object_detections
+
+    @property
+    def image_grey_blur(self) -> np.ndarray:
+        if not hasattr(self, "_image_grey_blur"):
+            grey = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+            self._image_grey_blur = cv2.GaussianBlur(grey, (5, 5), 0)
+        return self._image_grey_blur
+
+    def _detect_motion(self):
+
+        # start timing
+        start = datetime.now()
+        logger.debug(f"({self.hash}) Running motion detection")
+
+        # calculate mask of changes from the previous frame
+        diff = cv2.absdiff(self.prev_image_grey_blur, self.image_grey_blur)
+        _, diff_mask = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
+
+        # get mask of foreground from background removal model
+        fore_mask = BACK_SUB.apply(self.image)
+
+        # combine change and foreground masks
+        mask = cv2.bitwise_or(diff_mask, fore_mask)
+
+        # smooth regions
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.dilate(mask, kernel)
+
+        # store motion mask and presence flag
+        self._motion_mask = mask
+        self._has_motion = mask.mean() / 255 > 0.01
+
+        # log detection duration
+        elapsed = (datetime.now() - start).total_seconds()
+        logger.debug(f"({self.hash}) Motion detection duration: {elapsed*1000:.1f} ms")
+
+        # log motion
+        if self._has_motion:
+            logger.info(f"({self.hash}) Motion detected: {self._has_motion}")
+
+    @property
+    def motion_mask(self) -> np.ndarray:
+        if not hasattr(self, "_motion_mask"):
+            self._detect_motion()
+        return self._motion_mask
+
+    @property
+    def has_motion(self) -> bool:
+        if not hasattr(self, "_has_motion"):
+            self._detect_motion()
+        return self._has_motion
+
+    def _detect_objects(self):
+        # initialise detections output
+        self._object_detections = []
+
+        # only run detection if motion is present
+        if self.has_motion or self.prev_object_detections:
+            # start timing
+            start = datetime.now()
+            logger.debug(f"({self.hash}) Running object detection")
+
+            # run model inference
+            results = MODEL(
+                self.image,
+                imgsz=settings.IMGSZ,
+                verbose=False,
+                max_det=settings.MAX_DETS,
+            )[0]
+
+            # process detections
+            for r in results.boxes:
+                self._object_detections.append(
+                    {
+                        "box": r.xyxy[0].detach().cpu().int().tolist(),
+                        "conf": float(r.conf[0].item()),
+                        "class": int(r.cls[0].item()),
+                    }
+                )
+
+            # log detection duration
+            elapsed = (datetime.now() - start).total_seconds()
+            logger.debug(
+                f"({self.hash}) Object detection duration: {elapsed*1000:.1f} ms"
+            )
+
+            # identify excluded classes
+            self._has_excluded_class = bool(
+                {x["class"] for x in self._object_detections}.intersection(
+                    settings.EXCLUDED_CLASSES
+                )
+            )
+
+            # log detections
+            if self._object_detections:
+                logger.info(
+                    f"({self.hash}) Object(s) detected: {set([x["class"] for x in self._object_detections])}"
+                )
+
+        else:
+            self._has_excluded_class = False
+
+    @property
+    def object_detections(self) -> List[dict]:
+        # run object detection if not previously run
+        if not hasattr(self, "_object_detections"):
+            self._detect_objects()
+        return self._object_detections
+
+    @property
+    def has_excluded_class(self) -> bool:
+        # run object detection if not previously run
+        if not hasattr(self, "_has_excluded_class"):
+            self._detect_objects()
+        return self._has_excluded_class
+
+    @property
+    def image_annotated(self) -> np.ndarray:
+        # annotate image if not already done
+        if not hasattr(self, "_image_annotated"):
+            # start timing
+            start = datetime.now()
+            logger.debug(f"({self.hash}) Annotating image")
+
+            # copy image ready to be annotated
+            self._image_annotated = self.image.copy()
+
+            # loop over all detections
+            for obj in self.object_detections:
+
+                # unpack box coords
+                x1, y1, x2, y2 = obj["box"]
+
+                # draw bounding box
+                thickness = int(min(self._image_annotated.shape[:2]) / 250)
+                cv2.rectangle(
+                    self._image_annotated, (x1, y1), (x2, y2), ANN_COLOUR, thickness
+                )
+
+                # extract object class label/confidence and text size
+                label = f"{MODEL.names[obj["class"]]} {obj["conf"]:.2f}"
+                (w, h), _ = cv2.getTextSize(label, FONT, 1, 1)
+
+                # draw background rectangle for text
+                txt_box_coords = (int(x1 + 1.1 * w), int(y1 + 1.2 * h))
+                cv2.rectangle(
+                    self._image_annotated, (x1, y1), txt_box_coords, ANN_COLOUR, -1
+                )
+
+                # add text
+                txt_coords = (int(x1 + w * 0.05), int(y1 + h * 1.1))
+                cv2.putText(
+                    self._image_annotated, label, txt_coords, FONT, 1, (255, 255, 255)
+                )
+
+            # log annotation duration
+            elapsed = (datetime.now() - start).total_seconds()
+            logger.debug(
+                f"({self.hash}) Image annotation duration: {elapsed*1000:.1f} ms"
+            )
+
+        return self._image_annotated
+
+
+class PreBuffer:
+    def __init__(self, max_duration: Union[int, float]):
+        self.max_duration = max_duration
+        self.frames: List[Frame] = []
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.frames:
+            raise StopIteration
+        return self.frames.pop(0)
+
+    def __len__(self):
+        return len(self.frames)
+
+    def _sort(self):
+        self.frames.sort(key=lambda x: x.timestamp)
+
+    def check_duration(self, time):
+        min_time = time - timedelta(seconds=settings.BUFFER_DUR)
+        self.frames = [x for x in self.frames if x.timestamp >= min_time]
+        self._sort()
+
+    def put(self, frame: Frame):
+        self.frames.append(frame)
+        self.check_duration(frame.timestamp)
+
+
+def processing_thread():
+    """Process frames to detect objects and record videos"""
+    logger.info("Processing thread started")
+
+    # initialise preroll buffer
+    pre_buffer = PreBuffer(max_duration=settings.BUFFER_DUR)
+
+    # initialise state and previous frame
+    recording = False
+    writer = None
+    prev_image_grey_blur = np.zeros(
+        (settings.FRAME_HEIGHT, settings.FRAME_WIDTH), dtype=np.uint8
+    )
+    prev_object_detections = []  # type: ignore
+
+    while not shutdown_event.is_set() or not frame_queue.empty():
+
+        # get frame from capture queue
+        try:
+            timestamp, image = frame_queue.get(timeout=0.1)
+            frame = Frame(
+                timestamp=timestamp,
+                image=image,
+                prev_image_grey_blur=prev_image_grey_blur,
+                prev_object_detections=prev_object_detections,
+            )
+        except queue.Empty:
+            continue
+
+        # start timing
+        logger.debug(f"({frame.hash}) Running processing")
+        start = datetime.now()
+
+        if frame.object_detections:
+
+            if frame.has_excluded_class:
+
+                # clear buffer
+                pre_buffer.frames.clear()
+                logger.info("Clearing buffer due to detection of excluded class")
+
+            else:
+
+                # update latest detection timestamp
+                last_detection_time = frame.timestamp
+
+                # initialise recording and write pre buffer to video file
+                if not recording:
+                    out_path = os.path.join(
+                        settings.OUTPUT_DIR,
+                        f"{frame.timestamp.strftime('%Y%m%d_%H%M%S')}.mp4",
+                    )
+                    writer = cv2.VideoWriter(
+                        out_path, FOURCC, cam.fps, frame.image.shape[:2][::-1]
+                    )
+                    logger.warning(f"Starting recording: {out_path}")
+                    pre_buffer_len = len(pre_buffer)
+                    for bf in pre_buffer:
+                        writer.write(bf.image_annotated)
+                    logger.info(
+                        f"Written {pre_buffer_len} frames from pre detection buffer"
+                    )
+                    recording = True
+
+        if recording:
+
+            # write current frame and assess post buffer termination
+            if not frame.has_excluded_class:
+                writer.write(frame.image_annotated)
+                last_detection_dur = (
+                    frame.timestamp - last_detection_time
+                ).total_seconds()
+
+            # stop recording close video file
+            if (last_detection_dur > settings.BUFFER_DUR) or frame.has_excluded_class:
+                writer.release()
+                if last_detection_dur > settings.BUFFER_DUR:
+                    logger.info(
+                        f"Saving clip: last detection was {last_detection_dur:.3f} ago"
+                    )
+                elif frame.has_excluded_class:
+                    logger.info(f"Saving clip: excluded class detected")
+                recording = False
+
+        else:
+
+            # store current frame image and timestamp to rolling buffer
+            if not frame.has_excluded_class:
+                pre_buffer.put(frame)
+
+        # send frame to display queue
+        if SYSTEM == "Darwin":
+            try:
+                display_queue.put_nowait(frame.image_annotated.copy())
+            except queue.Full:
+                pass
+
+        # update current frame to be previous frame
+        prev_image_grey_blur = frame.prev_image_grey_blur.copy()
+        prev_object_detections = frame.prev_object_detections.copy()
+
+        # log processing rate
+        elapsed = (datetime.now() - start).total_seconds()
+        processing_fps = 1 / elapsed
+        logger.debug(f"({frame.hash}) Processing duration: {elapsed*1000:.1f} ms")
+        if processing_fps < cam.fps:
+            logger.warning(f"Processing thread slow: {processing_fps:.1f} FPS")
+
+    # cleanup
+    if writer is not None:
+        writer.release()
+        logger.info("Saving clip")
+
+    logger.info("Processing thread stopped")
