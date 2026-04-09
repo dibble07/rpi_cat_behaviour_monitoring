e7d8eaa9cb0745e139374a63582b6eea9d55904d
Robert Dibble <robertdibble@live.co.uk>
2026-04-09 17:16:07 +0100

    unify settings files

--- config.py
+++ config.py
@@ -6,22 +6,9 @@ from dynaconf import Dynaconf
 # establish current platform
 SYSTEM = system()
 
-# define path to plaform specific settings
-match SYSTEM:
-    case "Darwin":
-        _SETTINGS_PATH_PLATFORM = os.path.join(
-            os.path.dirname(__file__), "settings_mac.toml"
-        )
-    case "Linux":
-        _SETTINGS_PATH_PLATFORM = os.path.join(
-            os.path.dirname(__file__), "settings_pi.toml"
-        )
-    case _:
-        raise ValueError(f"Unexpected system: {SYSTEM}")
-
 # load settings
-_SETTINGS_PATH_GENEREAL = os.path.join(os.path.dirname(__file__), "settings.toml")
-settings = Dynaconf(settings_files=[_SETTINGS_PATH_GENEREAL, _SETTINGS_PATH_PLATFORM])
+_SETTINGS_PATH_GENERAL = os.path.join(os.path.dirname(__file__), "settings.toml")
+settings = Dynaconf(settings_files=_SETTINGS_PATH_GENERAL)
 settings.EXCLUDED_CLASSES = {
     int(x.strip()) for x in settings.EXCLUDED_CLASSES.split(",")
 }
