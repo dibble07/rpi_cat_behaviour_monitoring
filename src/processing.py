98c6153f1cb4804ad8dafd7b13971e4d8057c510
Robert Dibble <robertdibble@live.co.uk>
2026-07-01 20:53:00 +0100

    move bbox expansion to own function

--- src/processing.py
+++ src/processing.py
@@ -15,6 +15,7 @@ from ultralytics import YOLO
 
 from config import SYSTEM, settings
 from shared import cam, display_queue, frame_queue, shutdown_event
+import utils
 
 logger = logging.getLogger(__name__)
 
@@ -221,56 +222,10 @@ class Frame:
         # identify image size and mask coordinates
         h, w = self.image.shape[:2]
         ys, xs = np.where(self.motion_mask > 0)
-
-        # identify initial padded bounding box
-        pad = int(0.1 * h)
-        y1, y2 = max(0, int(ys.min()) - pad), min(h - 1, int(ys.max()) + pad)
-        x1, x2 = max(0, int(xs.min()) - pad), min(w - 1, int(xs.max()) + pad)
-
-        # calculate current and target aspect ratio
-        box_h = y2 - y1 + 1
-        box_w = x2 - x1 + 1
-        target_ar = w / h
-        box_ar = box_w / box_h
-
-        if box_ar == target_ar:
-
-            self._motion_bbox = None
-
-        else:
-
-            # calculate extra pixels needed and space either side
-            if box_ar < target_ar:
-                new_w = int(round(box_h * target_ar))
-                delta = new_w - box_w
-                space_bef, space_aft = x1, w - x2 - 1
-            elif box_ar > target_ar:
-                new_h = int(round(box_w / target_ar))
-                delta = new_h - box_h
-                space_bef, space_aft = y1, h - y2 - 1
-
-            # calculate growth either side, targetting symmetry but guaranteeing aspect ratio
-            if space_bef <= space_aft:
-                grow_bef = min(delta // 2, space_bef)
-                grow_aft = delta - grow_bef
-            else:
-                grow_aft = min(delta // 2, space_aft)
-                grow_bef = delta - grow_aft
-
-            # update bounding box locations
-            if box_ar < target_ar:
-                x1 -= grow_bef
-                x2 += grow_aft
-            elif box_ar > target_ar:
-                y1 -= grow_bef
-                y2 += grow_aft
-
-            # check aspect ratio is within rounding range
-            low_ar = (x2 - x1 + 0.5) / (y2 - y1 + 1.5)
-            high_ar = (x2 - x1 + 1.5) / (y2 - y1 + 0.5)
-            assert low_ar <= target_ar <= high_ar
-
-        self._motion_bbox = [x1, y1, x2, y2]
+        x_min, x_max, y_min, y_max = xs.min(), xs.max(), ys.min(), ys.max()
+        self._motion_bbox = utils.expand_bbox_from_bounds(
+            x_min, x_max, y_min, y_max, w, h, int(0.1 * h)
+        )
 
     @property
     def motion_bbox(self) -> Optional[list]:
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 
 
@@ -16,3 +18,62 @@ def get_best_device() -> torch.device:
         out = torch.device("cpu")
 
     return out
+
+
+def expand_bbox_from_bounds(
+    x_min: int,
+    x_max: int,
+    y_min: int,
+    y_max: int,
+    image_width: int,
+    image_height: int,
+    pad: int,
+) -> Optional[list[int]]:
+    """Expand a bbox with padding and enforce frame aspect ratio."""
+
+    # identify initial padded bounding box
+    y1, y2 = max(0, y_min - pad), min(image_height - 1, y_max + pad)
+    x1, x2 = max(0, x_min - pad), min(image_width - 1, x_max + pad)
+
+    # calculate current and target aspect ratio
+    box_h = y2 - y1 + 1
+    box_w = x2 - x1 + 1
+    target_ar = image_width / image_height
+    box_ar = box_w / box_h
+
+    # calculate extra pixels needed and space either side
+    if box_ar == target_ar:
+        return None
+    elif box_ar < target_ar:
+        new_w = int(round(box_h * target_ar))
+        delta = new_w - box_w
+        space_bef, space_aft = x1, image_width - x2 - 1
+    elif box_ar > target_ar:
+        new_h = int(round(box_w / target_ar))
+        delta = new_h - box_h
+        space_bef, space_aft = y1, image_height - y2 - 1
+    else:
+        raise ValueError(f"Cannot handle aspect ratios: {box_ar}, {target_ar}")
+
+    # calculate growth either side, targetting symmetry but guaranteeing aspect ratio
+    if space_bef <= space_aft:
+        grow_bef = min(delta // 2, space_bef)
+        grow_aft = delta - grow_bef
+    else:
+        grow_aft = min(delta // 2, space_aft)
+        grow_bef = delta - grow_aft
+
+    # update bounding box locations
+    if box_ar < target_ar:
+        x1 -= grow_bef
+        x2 += grow_aft
+    else:
+        y1 -= grow_bef
+        y2 += grow_aft
+
+    # check aspect ratio is within rounding range
+    low_ar = (x2 - x1 + 0.5) / (y2 - y1 + 1.5)
+    high_ar = (x2 - x1 + 1.5) / (y2 - y1 + 0.5)
+    assert low_ar <= target_ar <= high_ar
+
+    return [int(x1), int(y1), int(x2), int(y2)]
