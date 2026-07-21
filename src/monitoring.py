fec5c6e7f2929827d4fa1a67c55550f7bd5749a5
Robert Dibble <robertdibble@live.co.uk>
2026-07-21 12:04:03 +0100

    increase monitoring frequency

--- src/monitoring.py
+++ src/monitoring.py
@@ -48,4 +48,4 @@ def monitoring_thread() -> None:
             case _:
                 logger.debug(f"Disk write: {disk_write_mb:.0f} MB total")
 
-        time.sleep(settings.MONITORING_FREQ)
+        time.sleep(settings.MONITORING_PERIOD)
