a5f53ea4af921d3238ba77967f3cd7976963d492
Robert Dibble <robertdibble@live.co.uk>
2026-07-29 17:40:52 +0100

    update detection and classification models

--- models/yolo26n_subclass_ncnn_model/model_ncnn.py
+++ models/yolo26n_subclass_ncnn_model/model_ncnn.py
@@ -9,8 +9,8 @@ def test_inference():
     out = []
 
     with ncnn.Net() as net:
-        net.load_param("../models/yolo26n_subclass_ncnn_model/model.ncnn.param")
-        net.load_model("../models/yolo26n_subclass_ncnn_model/model.ncnn.bin")
+        net.load_param("models_staging/yolo26n_subclass_ncnn_model/model.ncnn.param")
+        net.load_model("models_staging/yolo26n_subclass_ncnn_model/model.ncnn.bin")
 
         with net.create_extractor() as ex:
             ex.input("in0", ncnn.Mat(in0.squeeze(0).numpy()).clone())
