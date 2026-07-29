d53dd683d7c237e243f91c84a4e95ef53ef2a77c
Robert Dibble <robertdibble@live.co.uk>
2026-07-29 17:02:14 +0100

    allow raw only recording

--- src/config.py
+++ src/config.py
@@ -9,6 +9,13 @@ SYSTEM = system()
 # load settings
 _SETTINGS_PATH_GENERAL = os.path.join(os.path.dirname(__file__), "settings.toml")
 settings = Dynaconf(settings_files=_SETTINGS_PATH_GENERAL)
+
+allowed_save_raw_video_modes = {"no", "only", "both"}
+if settings.SAVE_RAW_VIDEO not in allowed_save_raw_video_modes:
+    raise ValueError(
+        f"SAVE_RAW_VIDEO value {settings.SAVE_RAW_VIDEO} must be one of {allowed_save_raw_video_modes}"
+    )
+
 settings.EXCLUDED_OBJECTS = {
     x.strip() for x in settings.EXCLUDED_OBJECTS.split(",") if x.strip()
 }
