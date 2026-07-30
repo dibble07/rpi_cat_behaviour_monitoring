67a27610dee69546624c16a91be8b0b57b48ca3f
Robert Dibble <robertdibble@live.co.uk>
2026-07-30 09:23:56 +0100

    remove most diagnostic annotations

--- src/processing.py
+++ src/processing.py
@@ -388,16 +388,7 @@ class Frame:
                     )
 
                     # extract object/cat label, confidence, and text size
-                    name = summary.cat_name or track_frame.object_name
-                    cat_conf_str = (
-                        f"({summary.cat_conf*100:.0f}%/" if summary.cat_conf else "("
-                    )
-                    label = (
-                        f"{name} {summary.track_id} "
-                        f"{cat_conf_str}"
-                        f"{track_frame.confidence*100:.0f}%) "
-                        f"- {summary.state.name[0]}{summary.frame_count-summary.first_detection_index}"
-                    )
+                    label = f"{summary.track_id} {summary.cat_name or track_frame.object_name}"
                     (w, h), _ = cv2.getTextSize(label, FONT, 1, 1)
 
                     # draw background rectangle for text
