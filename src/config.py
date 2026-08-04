77ef93cbaa4f36e31237ad29a666a7e4f41453d7
Robert Dibble <robertdibble@live.co.uk>
2026-08-04 22:06:18 +0100

    match frame aspect ratio in detection model

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import os
+from math import ceil
 from platform import system
 
 from dynaconf import Dynaconf
@@ -10,12 +11,28 @@ SYSTEM = system()
 _SETTINGS_PATH_GENERAL = os.path.join(os.path.dirname(__file__), "settings.toml")
 settings = Dynaconf(settings_files=_SETTINGS_PATH_GENERAL)
 
+# check settings for validity
 allowed_save_raw_video_modes = {"no", "only", "both"}
 if settings.SAVE_RAW_VIDEO not in allowed_save_raw_video_modes:
     raise ValueError(
         f"SAVE_RAW_VIDEO value {settings.SAVE_RAW_VIDEO} must be one of {allowed_save_raw_video_modes}"
     )
 
+# process excluded objects
 settings.EXCLUDED_OBJECTS = {
     x.strip() for x in settings.EXCLUDED_OBJECTS.split(",") if x.strip()
 }
+
+# define scaled detection image size
+settings.DETECTION_IMGSZ = (
+    int(
+        ceil(
+            settings.DETECTION_IMGSZ_W
+            / settings.FRAME_WIDTH
+            * settings.FRAME_HEIGHT
+            / 32
+        )
+    )
+    * 32,
+    settings.DETECTION_IMGSZ_W,
+)
