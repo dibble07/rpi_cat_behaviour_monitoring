0d1b18892591566788702a8d4ab2d603abfaa6d8
Robert Dibble <robertdibble@live.co.uk>
2026-08-12 17:51:11 +0100

    drop ultralytics yolo

--- src/processing.py
+++ src/processing.py
@@ -15,7 +15,6 @@ from typing import List, Optional
 
 import cv2
 import numpy as np
-from ultralytics import YOLO
 
 import utils
 from config import SYSTEM, settings
@@ -29,26 +28,13 @@ logger = logging.getLogger(__name__)
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 
 # load object detection model
-MODEL = YOLO(Path("models") / settings.MODEL_DETECTION_PATH, task="detect")
+MODEL = YOLO_NCNN(Path("models") / settings.MODEL_DETECTION_PATH)
 _ = MODEL(
     np.zeros((settings.FRAME_HEIGHT, settings.FRAME_WIDTH, 3), dtype=np.uint8),
     imgsz=tuple(settings.DETECTION_IMGSZ),
     conf=settings.CONF,
     iou=settings.NMS_IOU_THRESHOLD,
-    verbose=False,
     max_det=settings.MAX_DETS,
-)
-
-# load NCNN model for benchmarking
-MODEL_NCNN = YOLO_NCNN(
-    Path("models") / settings.MODEL_DETECTION_PATH,
-    conf=settings.CONF,
-    iou=settings.NMS_IOU_THRESHOLD,
-    max_det=settings.MAX_DETS,
-)
-MODEL_NCNN(
-    np.zeros((settings.FRAME_HEIGHT, settings.FRAME_WIDTH, 3), dtype=np.uint8),
-    imgsz=tuple(settings.DETECTION_IMGSZ),
 )
 
 # define background subtractor
@@ -301,7 +287,6 @@ class Frame:
                 imgsz=tuple(settings.DETECTION_IMGSZ),
                 conf=settings.CONF,
                 iou=settings.NMS_IOU_THRESHOLD,
-                verbose=False,
                 max_det=settings.MAX_DETS,
             )[0]
 
@@ -322,17 +307,6 @@ class Frame:
 
             # log detection duration
             utils.log_timing(logger, "Object detection", start, self.hash)
-
-            # NCNN benchmarking (separate from main detection timing)
-            start = datetime.now()
-            MODEL_NCNN(
-                image,
-                imgsz=tuple(settings.DETECTION_IMGSZ),
-                conf=settings.CONF,
-                iou=settings.NMS_IOU_THRESHOLD,
-                max_det=settings.MAX_DETS,
-            )
-            utils.log_timing(logger, "NCNN model inference", start, self.hash)
 
             # log detections
             if track_frames:
